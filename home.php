11e56edb5aab8770b6e9cbb300bf09cb405804e1
Pavan Uthsara <uthsarapavan@gmail.com>
2024-04-25 19:54:28 +0530

    version 1

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php require_once('inc/header1.php')?>
+
+<body>
+	<div class="main-container">
+
+		<?php require_once('inc/header1-1.php') ?>
+
+		
+
+		<?php require_once('inc/footer.php') ?>
+	</div>
+
+
+</body>
+</html>
