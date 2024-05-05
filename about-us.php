5d1ab033fa7c75b3ba3de15fa79e82cbc22865dd
Pavan Uthsara <uthsarapavan@gmail.com>
2024-05-05 23:05:40 +0530

    develop home page

--- about-us.php
+++ about-us.php
@@ -10,22 +10,10 @@
 <body>
 	<div class="main-container">
 
-        <div class="head">
-            <br>
-            <h1>Shield Plus Insurance</h1>
-            <h5>Best health insurance partner</h5> <br>
-        </div>
+        <?php include_once('inc/header1-1.php'); ?>
 
-        <ul class="nav1">
-            <li class="nav1"><a href="home.php">Home</a></li>
-            <li class="nav1"><a href="about-us.php">About Us</a></li>
-            <li class="nav1"><a href="plans.php">Plans</a></li>
-            <li class="nav1"><a href="contact-us.php">Contact Us</a></li>
-            <li class="nav1"><a href="faq.php">FAQ</a></li>
-            <li class="nav2"><a href="sign-up.php">Sign Up</a></li>
-            <li class="nav2"><a href="sign-in.php">Sign In</a></li>
-        </ul>
-        
+        <img src="img/health_image.jpg" width="100%" height="400px">
+
         <div class="section">
         <!--<img src="health_image.jpg" width="100%" height="400px">-->
         <h1 id = "overview ">Overview</h1>
--- home.php
+++ home.php
@@ -1,13 +1,63 @@
-<?php require_once('inc/header1.php')?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home - Shield Plus</title>
+	<link rel="stylesheet" type="text/css" href="./css/home-2.css">
+</head>
 
 <body>
 	<div class="main-container">
 
-		<?php require_once('inc/header1-1.php') ?>
+	<?php include_once('inc/header1-1.php'); ?>
+	
+	<div class="main">
+		<img src="home-images/main.png" alt="main image">
+		<div class="mainText">
+			<h1>Your Trusted Partner in Health and Wellbeing</h1>
+			<p>We want to genuinely engage in your life, providing the right financial/insurance solutions for you and your family in a constantly changing world. Establish a sense of trust and reliability by positioning your insurance company as a dependable partner committed to the health and wellbeing of its members</p>
+			<button>Learn More</button>
+		</div>
+    </div>
 
+	<div class="footer">
+            <div class="text">
+                <h2>CONTACT</h2>
+                <p>+94 11 0000000</p>
+                
+                <p>Fax : (+94) 11 0000000</p>
+                <p>Email : lk.info@shieldplusinsurance.com</p>
+            </div>
+            <div class="text">
+                <h2>GO</h2>
+                <p>SHIELDPLUSINSURANCE.COM</p>
+                <p>Visit our corporate site to learn more about <br>shieldplusinsurance.</p>
+            </div>
+            <div class="text">
+                <h2>Head Office</h2>
+                <p>Shield Plus Insurance,<br>
+                    251, Malabe<br>
+                    Colombo<br>
+                    Sri Lanka.</p>
+            </div>
+            <div class="text">
+                <h2>Get Social with us on</h2>
+                <p>Facebook</p>
+                <p>Linkedin</p>
+                <p>Youtube</p>
+                <p>Twitter</p>
+            </div>
+
+        </div>
 		
 
-		<?php require_once('inc/footer.php') ?>
+	<footer>
+		<hr>
+		&copy; 2024 Copyright Reserved - Shield Plus Insurance <br>
+		<small>email@shieldplus.com</small>
+	</footer>
+
 	</div>
 
 
