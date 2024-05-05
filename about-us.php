6e386efbdb344b53502c8aafc856ddad21a621c7
Pavan Uthsara <uthsarapavan@gmail.com>
2024-05-05 17:47:12 +0530

    develop the about us page

---
+++ about-us.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home - Shield Plus</title>
+	<link rel="stylesheet" type="text/css" href="./css/about-us.css">
+</head>
+
+<body>
+	<div class="main-container">
+
+        <div class="head">
+            <br>
+            <h1>Shield Plus Insurance</h1>
+            <h5>Best health insurance partner</h5> <br>
+        </div>
+
+        <ul class="nav1">
+            <li class="nav1"><a href="home.php">Home</a></li>
+            <li class="nav1"><a href="about-us.php">About Us</a></li>
+            <li class="nav1"><a href="plans.php">Plans</a></li>
+            <li class="nav1"><a href="contact-us.php">Contact Us</a></li>
+            <li class="nav1"><a href="faq.php">FAQ</a></li>
+            <li class="nav2"><a href="sign-up.php">Sign Up</a></li>
+            <li class="nav2"><a href="sign-in.php">Sign In</a></li>
+        </ul>
+        
+        <div class="section">
+        <!--<img src="health_image.jpg" width="100%" height="400px">-->
+        <h1 id = "overview ">Overview</h1>
+        <p class = "header"><B>Who are we?</p></B></p>
+        <P>As a top supplier of health insurance management solutions, <b>Shield Plus Insurance</b> is committed to making sure that our customers are safe and secure. We work hard to provide both people and businesses with extensive coverage and individualised service by utilising state-of-the-art technology and a staff of seasoned professionals....</p>
+            <ol>
+                <p>Personalised programmes for both people and businesses.</p>
+                <p>Effective processing and payment of claims.</p>
+                <p>Committed customer service for quick resolution.</p> 
+                <p>vast network of medical professionals.</p>
+            </ol>
+
+            
+        <p class = "header"><B>What do we do? </B></p>
+        <P>Our speciality is providing cutting-edge health insurance management solutions that are customised to satisfy our clients' various needs. To optimise the delivery of healthcare benefits, <i>our platform combines effective claims management, smooth administrative procedures, and strong analytics</i>. We work together to bring financial security and peace of mind while navigating the intricacies of the healthcare landscape with businesses, individuals, and healthcare providers....</p>
+           
+        <p class = "header"><B>Vision... </B></p>
+        <P>Our goal is to completely transform the healthcare sector by providing easily accessible, transparent, and long-lasting health insurance options to both individuals and businesses. Our goal is to become the go-to partner for anyone looking for outstanding service, dependable coverage, and proactive health management....</p>
+        <p class = "header"><B>Mission... </B></p>
+        <P>Our goal at Shield Plus Insurance is to maximise value for our clients while streamlining the complex world of health insurance management. We are dedicated to providing cutting-edge solutions that improve healthcare access, encourage preventative care, and cultivate a well-being-oriented culture. We seek to go above and beyond expectations and positively influence the lives we come into contact with by cooperation, honesty, and constant growth....</p>
+        <br>
+
+	</div>
+    
+    <footer>
+        <hr>
+        &copy; 2024 Copyright Reserved - Shield Plus Insurance <br>
+        <small>email@shieldplus.com</small>
+    </footer>
+
+</body>
+</html>
